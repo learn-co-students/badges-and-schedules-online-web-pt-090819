cb427c1628353daa602425c61dc389e38f133729
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-11 03:25:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  newArray = []
+  array.each() { |element|
+    newArray.push("Hello, my name is #{element}.")
+  }
+  return newArray
+end 
+
+def assign_rooms(array)
+  newArray = []
+  count = 0
+  array.each() { |element| 
+    count += 1
+    newArray.push("Hello, #{element}! You'll be assigned to room #{count}!")
+  }
+  return newArray
+end 
+
+def printer(array)
+  batch_badge_creator(array).each() { |element| 
+    puts element
+  }
+  assign_rooms(array).each() { |element| 
+    puts element
+  }
+end
