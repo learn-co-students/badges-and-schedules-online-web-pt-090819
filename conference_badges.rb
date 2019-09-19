a997203e7b362f29d1dcd4bdc78c866722ac0e07
Janay Bailey <tjanay87@gmail.com>
2019-09-19 20:54:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+name =  ("Edsger","Ada","Charles","Alan","Grace","Linus","Matz")
+number = 0
+def batch_badger_creator()
+  name.each do |name|
+  puts "Hello, my name is #{name}."
+end
+
+def assign_rooms()
+  while batch_badger_creator()
+  puts "Hello #{name}! You'll be assigned to room #{number}!"
+  number += 1 
+end
